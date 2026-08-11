f393bcf830a25b75c4c708b270f53d4daec60686
Masyor <Masyor@users.noreply.github.com>
2026-08-11 02:28:28 +0000

    deploy: 6898894f65718064078e2fd73daee24737cdbd10

---
+++ config.js
@@ -0,0 +1,31 @@
+var constants = {
+  "config": {
+    "appTitle": "Imperial Tactica Construction Kit",
+    "appSub": "40k Tapestry",
+    "repository": "https://github.com/your-username/your-repo-name"
+  },
+  "colors": [
+    "#303030",
+    "#871b1b",
+    "#141464",
+    "#b98c1a",
+    "#336600",
+    "#7b2b7a",
+    "#F4F5F6"
+  ],
+  "tabs": [],
+  "backgrounds": [],
+  "fonts": [
+    {
+      "font": "Cinzel",
+      "size": 28,
+      "uppercase": true
+    },
+    {
+      "font": "Augusta",
+      "size": 35,
+      "uppercase": false
+    }
+  ],
+  "brushes": []
+};
